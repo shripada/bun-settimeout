37c04eda2115549d5b1f107405d6d19a28220ab9
Shripada Hebbar <shripada.hebbar@codecraft.co.in>
2022-07-20 21:24:52 +0530

    a sample js script that has few settimeout calls

---
+++ timeout.mjs
@@ -0,0 +1,3 @@
+setTimeout(() => console.log("I am called after 6 secs"), 6000);
+setTimeout(() => console.log("I am called after 4 secs"), 4000);
+console.log("I am called first in this async world!");
